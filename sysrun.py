eee5de9c73cafa29a339ea2d653c62cc3d2a2519
yxbaju <yxbaju@qq.com>
2019-06-22 14:00:21 +0800

    ADD GLOBAL VARIANT

--- sysrun.py
+++ sysrun.py
@@ -1,8 +1,10 @@
-from ui_login import Ui_Login
-from mainform import MainForm
-from PyQt5.QtWidgets import QMainWindow, QMessageBox, QWidget, QApplication
 import sys
 import getdb
+import user_info
+
+from mainform import *
+from ui_login import Ui_Login
+from PyQt5.QtWidgets import QMessageBox, QWidget, QApplication, QMainWindow
 
 
 class Login(QMainWindow, Ui_Login):
@@ -15,14 +17,22 @@ class Login(QMainWindow, Ui_Login):
         self.btn_quit.clicked.connect(sys.exit)
 
     def login(self):
-        username, password = self.line_username.text(), self.line_password.text()
-        if username and password:
+        USERNAME, password = self.line_username.text(), self.line_password.text()
+        if USERNAME and password:
             db = getdb.AccDb()
             with db:
-                sql = "select 密码 from 用户 where 用户名='%s'" % username
+                sql = "select 密码,部门,邮箱 from 用户 where 用户名='%s'" % USERNAME
                 rst = db.get_rst(sql)
             if rst and rst[0][0] == password:
+                PART = rst[0][1]
+                EMAIL = rst[0][2]
+                user_info._init()
+                user_info.set_value('USERNAME', USERNAME)
+                user_info.set_value('PART', PART)
+                user_info.set_value('EMAIL', EMAIL)
+
                 main_win.showMaximized()  # login success then transfer to Main Form and close login form
+                main_win.setWindowTitle(main_win.windowTitle() + '   >>>当前用户：<%s> %s' % (PART, USERNAME))
                 self.close()
             else:
                 self.show_msg('warning', 'Error', '用户名或密码错误!')
