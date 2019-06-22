d6d7826970d28dab3184ac01dd8de5e84a1da86e
yxbaju <yxbaju@qq.com>
2019-06-22 13:56:43 +0800

    ADD GLOBAL VARIANT

---
+++ user_info.py
@@ -0,0 +1,14 @@
+"""global variant"""
+
+
+def _init():
+    global _global_dict
+    _global_dict = {}
+
+
+def set_value(key,value):
+    _global_dict[key] = value
+
+
+def get_value(key):
+    return _global_dict.get(key, None)
